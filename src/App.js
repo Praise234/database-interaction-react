6770403356458b345b49f38b5e49ccf05d013e9a
Ayanwale Mofiyinfoluwa Praise <praise207@yahoo.com>
2023-08-12 15:21:38 +0100

    Add final code

--- src/App.js
+++ src/App.js
@@ -13,22 +13,21 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-218f6-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedData = [];
+
+      for(const key in  data){
+        loadedData.push({id: key, title: data[key].title, openingText: data[key].openingText, releaseDate: data[key].releaseDate});
+      }
+
+   
+      setMovies(loadedData);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +38,27 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    try {
+
+      const response = await fetch('https://react-http-218f6-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // const data = await response.json();
+    if(response.ok){
+      fetchMoviesHandler();
+    }else{
+      throw new Error('Something went wrong!');
+    }
+      
+    } catch (error) {
+      setError(error);
+    }
   }
 
   let content = <p>Found no movies.</p>;
